944b7b83eafd05a9278e09913f2bc5a6b4b32bd1
RupasreeH <hroopa015@gmail.com>
2025-03-11 23:50:14 -0500

    rp - done

--- src/app/content-child/content-child.component.ts
+++ src/app/content-child/content-child.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-content-child',
   imports: [],
+  standalone: true,
   templateUrl: './content-child.component.html',
   styleUrl: './content-child.component.css',
 })
-export class ContentChildComponent {}
+export class ContentChildComponent {
+  @ContentChild('valueFromParent') valueFromParent: ElementRef | undefined;
+
+  ngAfterContentInit() {
+    console.log(this.valueFromParent?.nativeElement.textContent);
+  }
+  ngAfterContentChecked() {
+    console.log(this.valueFromParent?.nativeElement.textContent);
+  }
+}
